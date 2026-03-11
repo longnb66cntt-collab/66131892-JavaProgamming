58670ae4092dc138096b68234075aa4a0a6033a7
longnb66cntt-collab <long.nb.66cntt@ntu.edu.vn>
2026-03-11 11:28:49 +0700

    nhập biến

--- Bai1_Nhapxuat_tinhtong2So/src/tong2sola.java
+++ Bai1_Nhapxuat_tinhtong2So/src/tong2sola.java
@@ -1,9 +1,23 @@
+import java.util.Scanner;
 
 public class tong2sola {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//khai báo
+		double num1, num2, sum;
+		Scanner banphim = new Scanner(System.in);
+		System.out.print("Mời nhập số thứ nhất");
+		num1 = banphim.nextDouble();
+		System.out.print("Mời nhập số thứ hai");
+		num2 = banphim.nextDouble();
+		sum = num1+num2;
+	//	System.out.print("Tổng của" + a + "và" + b + "là" + sum);
+		System.out.print(" Tổng của ");
+		System.out.print(num1);
+		System.out.print("và");
+		System.out.print(num2);
+		System.out.print( "là");
+		System.out.print(sum);
 	}
 
 }
