2f2a0fdbf2ae0b09465bca60964bd9e0cc887d86
longnb66cntt-collab <long.nb.66cntt@ntu.edu.vn>
2026-03-11 14:05:35 +0700

    thong tin SV

---
+++ OOPSinhVien/src/ChuongTrinhJava.java
@@ -0,0 +1,13 @@
+
+public class ChuongTrinhJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        SinhVien sv1 = new SinhVien();
+        sv1.mssv="66131892";
+        sv1.ten="Nguyen Bao Long";
+        sv1.tuoi=19;
+        sv1.hocBai("Lap Trinh Java");
+	}
+
+}
