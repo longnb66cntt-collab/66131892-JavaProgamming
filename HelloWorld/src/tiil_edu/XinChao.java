872a023d31ea22167475f926635b3b178330717f
longnb66cntt-collab <long.nb.66cntt@ntu.edu.vn>
2026-03-11 10:44:51 +0700

    Cập nhật code
    Sửa mới và thêm dòng

--- HelloWorld/src/tiil_edu/XinChao.java
+++ HelloWorld/src/tiil_edu/XinChao.java
@@ -4,8 +4,8 @@ public class XinChao {
 
 	public static void main(String[] args) {
 		//
-		System.out.println("TÔI LẦN ĐẦU TIÊN HỌC JAVA, và đang thử qunar lý mã nguồn GIT ");
-
+		System.out.println("TÔI LẦN ĐẦU TIÊN HỌC Java, và đang thử qunar lý mã nguồn GIT ");
+        System.out.print("Tôi thêm một dòng mới và sửa chử Java ở trên");
 	}
 
 }
