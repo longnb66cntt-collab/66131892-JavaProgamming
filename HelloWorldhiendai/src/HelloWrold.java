743667f0972871571e99f8c95615fcc75f27c0f4
longnb66cntt-collab <long.nb.66cntt@ntu.edu.vn>
2026-03-11 13:59:34 +0700

    baith1

---
+++ HelloWorldhiendai/src/HelloWrold.java
@@ -0,0 +1,14 @@
+
+public class HelloWrold {
+
+	public static void main(String[] args) {
+		var message = "Hello, world with var!";
+		var number = 42;
+		var list = java.util.List.of("Java", "moder");
+		System.out.println(message);
+		System.out.println("Number: " + number);
+		Sysrem.out.println("List: " + list);
+
+	}
+
+}
