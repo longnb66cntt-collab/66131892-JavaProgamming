9f3c8c51c488817561f554551d08192bf52d1ebf
longnb66cntt-collab <long.nb.66cntt@ntu.edu.vn>
2026-03-11 10:39:44 +0700

    Ví dụ đầu tiên
    Chỉ để demo

---
+++ HelloWorld/src/tiil_edu/XinChao.java
@@ -0,0 +1,11 @@
+package tiil_edu;
+
+public class XinChao {
+
+	public static void main(String[] args) {
+		//
+		System.out.println("TÔI LẦN ĐẦU TIÊN HỌC JAVA, và đang thử qunar lý mã nguồn GIT ");
+
+	}
+
+}
