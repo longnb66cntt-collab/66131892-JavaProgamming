4e2d9052d90e9ee1a84f234441bd5eee999060f8
longnb66cntt-collab <long.nb.66cntt@ntu.edu.vn>
2026-03-13 06:40:48 +0700

    sửa

--- VIDU2_OOP_Taoham_Getter/src/run.java
+++ VIDU2_OOP_Taoham_Getter/src/run.java
@@ -2,8 +2,24 @@
 public class run {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		ChuNhat cn1 = new ChuNhat();
+		ChuNhat cn2 = new ChuNhat(20,15);
+		ChuNhat hv = new ChuNhat(20,20);
+		 double cn1_dai=  cn1.getDai();
+		 double cn1_rong= cn1.getRong();
+		 
+		 System.out.print("Hinh CN1 co chieu dai la :");
+		 System.out.print(cn1_dai);
+		 System.out.print("\nHinh CN! co chieu rong la : " + cn1_rong);
+		 
+		 cn1.setDai(50);
+		 System.out.print("Sau khi thay doi chieu dai \n");
+		 System.out.print("Hinh CN1 co chieu dai la :");
+		 System.out.print(cn1_dai);
+		 System.out.print("\nHinh CN! co chieu rong la : " + cn1_rong);
+		 System.out.print( cn1.toString());
+		
 	}
 
 }
