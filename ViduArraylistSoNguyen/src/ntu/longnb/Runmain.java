a2db2d44cf4905ec743936a9f291944e841a6345
longnb66cntt-collab <long.nb.66cntt@ntu.edu.vn>
2026-03-16 12:08:01 +0700

    chức năng xuất danh sách

--- ViduArraylistSoNguyen/src/ntu/longnb/Runmain.java
+++ ViduArraylistSoNguyen/src/ntu/longnb/Runmain.java
@@ -9,11 +9,19 @@ public class Runmain {
 	public static void main(String[] args) {
 	      ArrayList<Integer> dsSoNguyen = new ArrayList<Integer>();
 	      Scanner banPhim = new Scanner(System.in);
-	      for ( int i=0; i<10; i++) {
+	   for ( int i=0; i<10; i++) {
 	      System.out.print("Mời nhập phần tử số " + i + ": ");
 	      Integer tam = banPhim.nextInt();
 	      dsSoNguyen.add(tam);
 	      }
+//	   for ( int i=0; i<dsSoNguyen.size(); i++) {
+        System.out.print("DANH SÁCH VỪA NHẬP LÀ \n"); 
+//		  Integer tam = dsSoNguyen.get(i);
+//	      System.out.print(tam + "");
+//	      }
+       for (Integer x : dsSoNguyen) {
+    	  System.out.print(x + "");
+       }
 	}
 
 }
