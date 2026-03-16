e738c59b8614c907f11e75a78d720e58481ef12c
longnb66cntt-collab <long.nb.66cntt@ntu.edu.vn>
2026-03-16 11:52:49 +0700

    câu a

--- ViduArraylistSoNguyen/src/ntu/longnb/Runmain.java
+++ ViduArraylistSoNguyen/src/ntu/longnb/Runmain.java
@@ -1,10 +1,19 @@
 package ntu.longnb;
 
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Runmain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	      ArrayList<Integer> dsSoNguyen = new ArrayList<Integer>();
+	      Scanner banPhim = new Scanner(System.in);
+	      for ( int i=0; i<10; i++) {
+	      System.out.print("Mời nhập phần tử số " + i + ": ");
+	      Integer tam = banPhim.nextInt();
+	      dsSoNguyen.add(tam);
+	      }
 	}
 
 }
