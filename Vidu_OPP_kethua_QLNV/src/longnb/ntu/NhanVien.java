51e97c4bfac019bb7e45e38c989e83f68d3c45d0
longnb66cntt-collab <long.nb.66cntt@ntu.edu.vn>
2026-03-19 09:44:04 +0700

    Viết main test lớp NV

--- Vidu_OPP_kethua_QLNV/src/longnb/ntu/NhanVien.java
+++ Vidu_OPP_kethua_QLNV/src/longnb/ntu/NhanVien.java
@@ -1,10 +1,41 @@
 package longnb.ntu;
 
 public class NhanVien {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	private String maNV;
+	private String hoTen;
+	private double luongCoBan;
+	//hamtao
+//	public NhanVien() {
+		
+//	}
+//   public NhanVien( String maNhanVien, String tenNhanVien, double luongCoBan) {
+//      maNV = maNhanVien;
+//      hoTen = tenNhanVien;
+ //     this.luongCoBan = luongCoBan;
+//	}
+	public NhanVien(String maNV, String hoTen, double luongCoBan) {
+		super();
+		this.maNV = maNV;
+		this.hoTen = hoTen;
+		this.luongCoBan = luongCoBan;
 	}
-
+	public NhanVien() {
+		super();
+	}
+	public double tinhLuong() {
+		return luongCoBan;
+	}
+	@Override
+	public String toString() {
+		String chuoiXuat="";
+		chuoiXuat = chuoiXuat + "MÃ NV: " + this.maNV;
+		chuoiXuat = chuoiXuat + "\nHọ tên: " + this.hoTen;
+		chuoiXuat = chuoiXuat + "\nLương cơ bản: " +this.luongCoBan;
+		return "NhanVien [maNV=" + maNV + ", hoTen=" + hoTen + ", luongCoBan=" + luongCoBan + "]";
+	}
+	public void hienthiThongTin() {
+       System.out.println("MÃ NV: " + maNV);
+       System.out.println("Họ Tên: " + hoTen);
+       System.out.println("Lương Cơ bản: " + luongCoBan);
+     }
 }
---
+++ Vidu_OPP_kethua_QLNV/src/longnb/ntu/Run.java
@@ -0,0 +1,11 @@
+package longnb.ntu;
+
+public class Run {
+
+	public static void main(String[] args) {
+		NhanVien nv1 = new NhanVien("001", "Mai Cường Thọ", 230000);
+		nv1.hienthiThongTin();
+
+	}
+
+}
