d732044ac051f38c765b6ae418a71d3af3d4ed46
longnb66cntt-collab <long.nb.66cntt@ntu.edu.vn>
2026-03-23 00:08:05 +0700

    viết code kế thừa hình chử nhật

---
+++ VD_OOP_ kethua_hinhoc/src/long_ntu/run.java
@@ -0,0 +1,12 @@
+package long_ntu;
+
+public class run {
+	public static void main(String[] args) {
+		hinhhoc hh1 = new hinhhoc("lục giác ","màu đỏ");
+		hh1.hienthithongtin();
+		hinhchunhat hcn1= new hinhchunhat(" hình chử nhật", "màu xanh ",3,2);
+		hcn1.hienthithongtin();
+		
+	}
+
+}
