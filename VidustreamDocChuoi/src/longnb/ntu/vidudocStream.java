c07c659f6503ccc8b01e3e5a060cd8359d6b04aa
longnb66cntt-collab <long.nb.66cntt@ntu.edu.vn>
2026-04-16 00:35:58 +0700

    đọc 1 chuỗi

--- VidustreamDocChuoi/src/longnb/ntu/vidudocStream.java
+++ VidustreamDocChuoi/src/longnb/ntu/vidudocStream.java
@@ -1,9 +1,18 @@
 package longnb.ntu;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class vidudocStream {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws IOException {
+		
+		InputStreamReader ipReader =new InputStreamReader(System.in);
+		BufferedReader bufReader = new BufferedReader(ipReader);
+		System.out.print("nhập một chuỗi bất kì: ");
+		String lineDocDuoc = bufReader.readLine();
+		System.out.print(lineDocDuoc);
 
 	}
 
