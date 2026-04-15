411f4ad929187083685994dc5a36704a3619b564
longnb66cntt-collab <long.nb.66cntt@ntu.edu.vn>
2026-04-16 01:34:04 +0700

    bai75

--- Baitap75/src/long_ntu/bai75.java
+++ Baitap75/src/long_ntu/bai75.java
@@ -1,9 +1,20 @@
 package long_ntu;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class bai75 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws IOException {
+			FileReader fr = new FileReader("bai75.txt");
+			BufferedReader br = new BufferedReader(fr);
+			String s;
+			while((s = br.readLine()) != null)
+			{
+			System.out.println(s);
+			}
+			fr.close();
 
 	}
 
