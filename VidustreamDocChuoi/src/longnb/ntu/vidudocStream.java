fea2e59d50c81f5e9af2cfb771595d1a78b3fef9
longnb66cntt-collab <long.nb.66cntt@ntu.edu.vn>
2026-04-16 00:47:43 +0700

    doc file

--- VidustreamDocChuoi/src/longnb/ntu/vidudocStream.java
+++ VidustreamDocChuoi/src/longnb/ntu/vidudocStream.java
@@ -1,18 +1,21 @@
 package longnb.ntu;
 
 import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class vidudocStream {
 
-	public static void main(String[] args) throws IOException {
-		
-		InputStreamReader ipReader =new InputStreamReader(System.in);
+	public static void main(String[] args) throws IOException { 
+		FileInputStream fInput = new FileInputStream("baitho.txt");
+		InputStreamReader ipReader =new InputStreamReader(fInput);
 		BufferedReader bufReader = new BufferedReader(ipReader);
-		System.out.print("nhập một chuỗi bất kì: ");
-		String lineDocDuoc = bufReader.readLine();
-		System.out.print(lineDocDuoc);
+		String line1 = bufReader.readLine();
+		System.out.print(line1);
+		String line2 = bufReader.readLine();
+		System.out.print(line2);
+		
 
 	}
 
