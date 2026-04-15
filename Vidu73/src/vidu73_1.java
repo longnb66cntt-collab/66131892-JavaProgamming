f737bb9d5e9347c5171dca4db0c7438d3179af84
longnb66cntt-collab <long.nb.66cntt@ntu.edu.vn>
2026-04-16 01:10:00 +0700

    bài 73.1

--- Vidu73/src/vidu73_1.java
+++ Vidu73/src/vidu73_1.java
@@ -1,8 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class vidu73_1 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws IOException {
+		InputStreamReader iputSR = new InputStreamReader(System.in);
+		BufferedReader buf = new BufferedReader(iputSR);
+		String str;
+		System.out.println("Nhap chuoi:");
+		System.out.println("Nhap 'stop' ket thuc chuong trinh.");
+		do
+		{
+		str = buf.readLine();
+		System.out.println(str);
+		} while(!str.equals("stop")); 
 
 	}
 
